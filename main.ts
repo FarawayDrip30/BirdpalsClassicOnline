e0f3ac0785fda0f8eb4ae8266cdca4ecd6b175fb
FarawayDrip30 <66689307+FarawayDrip30@users.noreply.github.com>
2025-12-01 18:46:49 +0000

    site now statically hosted using Oak

--- main.ts
+++ main.ts
@@ -1,7 +1,37 @@
-import express from "express";
+import { Application, send } from "@oak/oak";
+import { Server } from "socket.io";
 
-const app = express();
-app.use(express.static("public"));
+const app = new Application();
 
-app.listen(8000);
+const ROOT_DIR = "public"
+app.use(async (ctx) => {
+  await ctx.send({
+      root: ROOT_DIR,
+      index: "index.html"
+    });
+});
+
+
+const io = new Server();
+
+io.on("connection", (socket) =>{
+  console.log("SOCKET CONNECTED");
+
+  socket.emit("hello", "world");
+
+  socket.on("disconnect", (reason) => {
+    console.log("Player disconnected");
+  })
+})
+
+
+const handler = io.handler(async (req) => {
+  return await app.handle(req) || new Response(null, { status: 404});
+});
+
+Deno.serve({
+  handler: handler,
+  port: 8000
+})
+
 console.log("Running on http://localhost:8000");
